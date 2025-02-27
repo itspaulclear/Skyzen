fc8c4a2c8a7dd079828206319b8e1c90233480be
Pablo García <159904282+itspaulclear@users.noreply.github.com>
2025-02-27 18:15:24 +0100

    Delete SpringSecurityApp directory

--- SpringSecurityApp/SpringSecurityApp/src/main/java/com/app/controller/dto/AuthLoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.app.controller.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record AuthLoginRequest(@NotBlank String username,
-                               @NotBlank String password) {
-}
