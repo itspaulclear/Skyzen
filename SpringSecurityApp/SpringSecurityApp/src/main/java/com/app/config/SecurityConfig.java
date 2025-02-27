d5d3eeb54a78ff0d1f4486b5c75ed3c341959347
itspaulclear <pagarov@gmail.com>
2025-02-27 18:11:52 +0100

    Initial commit

---
+++ SpringSecurityApp/SpringSecurityApp/src/main/java/com/app/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.app.config;
+
+import com.app.config.filter.JwtTokenValidator;
+import com.app.service.UserDetailServiceImpl;
+import com.app.util.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	@Autowired
+	private JwtUtils jwtUtils;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+				.csrf(csrf -> csrf.disable())
+				.httpBasic(Customizer.withDefaults())
+				.formLogin(Customizer.withDefaults())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(http -> {
+					http.requestMatchers(HttpMethod.POST, "/auth/**").permitAll();
+					http.requestMatchers(HttpMethod.POST, "/method/post").hasAnyRole("ADMIN", "DEVELOPER");
+					http.requestMatchers(HttpMethod.DELETE, "/method/delete/**").hasAnyRole("ADMIN", "DEVELOPER");
+					http.requestMatchers(HttpMethod.PATCH, "/method/patch").hasAnyAuthority("REFACTOR");
+					http.requestMatchers(HttpMethod.GET, "/method/get").permitAll();
+					http.anyRequest().authenticated();
+				})
+				.addFilterBefore(new JwtTokenValidator(jwtUtils), BasicAuthenticationFilter.class) // Quitar para verificación con usuario y contraseña
+				.build();
+	}
+
+	/*
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+				.csrf(csrf -> csrf.disable())
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.build();
+	}
+	*/
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider(UserDetailServiceImpl userDetailService) {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(passwordEncoder());
+		provider.setUserDetailsService(userDetailService);
+		return provider;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		// return NoOpPasswordEncoder.getInstance(); (para pruebas)
+		return new BCryptPasswordEncoder();
+	}
+
+//	public static void main(String[] args) {
+//		System.out.println(new BCryptPasswordEncoder().encode("1234"));
+//	}
+}
---
+++ SpringSecurityApp/SpringSecurityApp/src/main/java/com/app/persistence/entity/PermissionEntity.java
@@ -0,0 +1,24 @@
+package com.app.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "permissions")
+public class PermissionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private String name;
+}
